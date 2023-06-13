d462fd56bf030a52c9ad91ceef4f4b14be6d0119
huy.pham <huy.pham@opes.com.vn>
2023-06-13 15:05:14 +0700

    refactor: Run worker on aplication bootstrap

--- worker/src/worker/worker.service.ts
+++ worker/src/worker/worker.service.ts
@@ -1,44 +1,42 @@
-import { Injectable, OnModuleInit } from "@nestjs/common";
-import { ConsumerService } from "../kafka/consumer.service";
+import { Injectable, OnApplicationBootstrap } from "@nestjs/common";
 import { EachMessagePayload } from "kafkajs";
-import { RedisService } from "../redis/redis.service";
-import { MongodbService } from "../mongodb/mongodb.service";
 import { TrackDTO } from "../dto/track.dto";
 import { Location } from "../entities/location.entity";
+import { ConsumerService } from "../kafka/consumer.service";
+import { MongodbService } from "../mongodb/mongodb.service";
+import { RedisService } from "../redis/redis.service";
 
 @Injectable()
-export class WorkerService {
+export class WorkerService implements OnApplicationBootstrap {
     constructor(
         private kafkaConsumer: ConsumerService,
         private redisService: RedisService,
         private mongodbService: MongodbService
-    ) {
-        this.kafkaConsumer.readMessage(
-            "default_topic",
-            async (payload: EachMessagePayload): Promise<void> => {
-                try {
-                    const trackDTO: TrackDTO = JSON.parse(
-                        payload.message.value?.toString() || "{}"
-                    );
-                    console.log(this.redisService);
-                    const redisClient = this.redisService.getInstance();
-                    const l: Location = {
-                        latitude: trackDTO.latitude,
-                        longtitude: trackDTO.longtitude,
-                    };
-                    await Promise.all([
-                        redisClient.LPUSH(
-                            "locations",
-                            JSON.stringify(trackDTO)
-                        ),
-                        this.mongodbService
-                            .getCollection<Location>("track", "location")
-                            .insertOne(l),
-                    ]);
-                } catch (error) {
-                    console.log(error);
-                }
+    ) {}
+
+    onApplicationBootstrap() {
+        const readMessage = async (
+            payload: EachMessagePayload
+        ): Promise<void> => {
+            try {
+                const trackDTO: TrackDTO = JSON.parse(
+                    payload.message.value?.toString() || "{}"
+                );
+                const redisClient = this.redisService.getInstance();
+                const l: Location = {
+                    latitude: trackDTO.latitude,
+                    longtitude: trackDTO.longtitude,
+                };
+                await Promise.all([
+                    redisClient.LPUSH("locations", JSON.stringify(trackDTO)),
+                    this.mongodbService
+                        .getCollection<Location>("track", "location")
+                        .insertOne(l),
+                ]);
+            } catch (error) {
+                console.log(error);
             }
-        );
+        };
+        this.kafkaConsumer.readMessage("default_topic", readMessage);
     }
 }
